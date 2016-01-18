5ac7d918389a2aed532d0f0a8e68dbc95a6ef82a
Adam Jackman <adam@acjackman.com>
2016-01-18 02:21:58 -0700

    Simplify print cred tests

--- tests/testthat/test_print_cred.R
+++ tests/testthat/test_print_cred.R
@@ -1,13 +1,11 @@
 context("db_credentials - print.cred")
 
-test_that("cred objects print properly", {
-    crd <- cred("bob", "passwd", "127.0.0.1", "foo", 3306)
+crd <- cred("bob", "passwd", "127.0.0.1", "foo", 3306)
 
+test_that("cred objects print properly", {
     expect_output(crd, "db_cred: bob@127.0.0.1:3306/foo")
 })
 
 test_that("cred objects print with paswd", {
-    crd <- cred("bob", "passwd", "127.0.0.1", "foo", 3306)
-
     expect_output(crd, "db_cred: bob@127.0.0.1:3306/foo")
 })
