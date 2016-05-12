b3d6b365a4a6cc4edd675f5f97c55022a21836f5
Adam Jackman <adam@acjackman.com>
2016-05-12 17:04:10 -0600

    qlog functions to force logging

--- R/log_query.R
+++ R/log_query.R
@@ -10,15 +10,33 @@
 log_query <- function(query, .db){
     # Determine if the query should be logged
     force_log <- attr(.db, "force_log")
-    if (is.null(force_log)){
-        log_query <- getOption("dbezr.force_log")
-    } else{
-        log_query <- force_log
+    if (is.na(force_log)){
+        force_log <- getOption("dbezr.force_log")
     }
 
     # Log the query
-    if (log_query){
+    if (force_log){
         cat("Running Query (", format(lubridate::now()), " - ",
             cred_id(.db), "):\n", query, ";\n", sep = "")
   }
+}
+
+#' Force queries logging
+#'
+#' Force queries to be logged or not, regardless of the other settings.
+#' @param .db credentials used for this query, if \code{NULL}, default is used
+#' @rdname cred
+#' @export
+qlog_loud <- function(.db = NULL){
+    .db <- get_db(.db)
+    attr(.db, "force_log") <- TRUE
+    .db
+}
+
+#' @rdname cred
+#' @export
+qlog_silent <- function(.db = NULL){
+    .db <- get_db(.db)
+    attr(.db, "force_log") <- FALSE
+    .db
 }
