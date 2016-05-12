8a9a2f6e9daa3f650542a1788ac9e0a3a0b398f7
Adam Jackman <adam@acjackman.com>
2016-05-12 12:18:02 -0600

    Correct tests to direct output to be checked

--- tests/testthat/test_print_cred.R
+++ tests/testthat/test_print_cred.R
@@ -3,9 +3,9 @@ context("db_credentials - print.cred")
 crd <- cred("bob", "passwd", "127.0.0.1", "foo", 3306)
 
 test_that("cred objects print properly", {
-    expect_output(crd, "db_cred: bob@127.0.0.1:3306/foo")
+    expect_output(print(crd), "db_cred: bob@127.0.0.1:3306/foo")
 })
 
 test_that("cred objects print with paswd", {
-    expect_output(crd, "db_cred: bob@127.0.0.1:3306/foo")
+    expect_output(print(crd), "db_cred: bob@127.0.0.1:3306/foo")
 })
